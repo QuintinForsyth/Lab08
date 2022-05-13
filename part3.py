a558eb2b10f2d1a5268053df78293f234c610368
Quintin Forsyth <100383529+QuintinForsyth@users.noreply.github.com>
2022-05-13 01:13:25 +0000

    fixed it and it looks nice

--- part3.py
+++ part3.py
@@ -2,7 +2,7 @@ import summonFile as s
 
 people = s.csv21list("people.csv")
 
-def count_country(people):
+def country_count(people):
   """
   counts contries
   """
@@ -17,7 +17,13 @@ def count_country(people):
           f = countries.index(i)
           f=f+1
           countries[f] = countries[f] +1
-          
-    
-  print(countries)
-count_country(people)
+
+  f = 0
+  for i in countries:
+      f = countries.index(i)
+      f+=1
+      if f %2 == 0:
+        continue
+      print(f"We have {countries[f]} from {i} attending this Class")
+
+country_count(people)
